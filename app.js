c455aa239dc1b088f208871f857c15f246702a7a
Luis Diniz <luisdiniz@infosistemas.com.br>
2018-09-17 19:06:09 -0300

    commit temporário

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 // Imports
 const interImport = require('./import/Inter');
+const lancamentoRepository = require('./repository/lancamentoRepository');
 const sql = require('./util/db');
 // Constantes
 const consultarConta = `SELECT * FROM Conta`;
-const consultarLancamentos = `SELECT * FROM Lancamento`;
 const filePath = '/Users/Luis/Downloads/inter-extrato.csv'
 
 async function main() {
@@ -16,26 +16,26 @@ async function main() {
     for (let record of result.recordset) {
         console.log(record);            
     }
-    console.log(`\nLançamento:\n`);
-    result = await sql.query(consultarLancamentos);
-    for (let record of result.recordset) {
+    console.log(`\nLançamentos:\n`);
+    result = await lancamentoRepository.findAll();
+    for (let record of result.recordset)
         console.log(record);            
-    }
 
     // Teste importar extrato banco Inter
     let retorno = await interImport.importFileLancamento(filePath);
+    // Remover da lista lancamentos que já existem no banco    
+    for (let lancamento of retorno.lancamentos)
+        if (lancamentoRepository.findLancamentoDuplicado(lancamento.descricao, lancamento.valor, lancamento.data))
+            retorno.lancamentos.pop(lancamento);
+    // Persiste as informações
+    // contaRepository.atualizarSaldo(contaId, retorno.saldo);
+    // Insere os novos lancamentos no banco
     for (let lancamento of retorno.lancamentos){
-        await sql.query(`INSERT INTO Lancamento (
-                            DescricaoOriginal,
-                            Valor,
-                            Data
-                          )
-                          VALUES (
-                            ${lancamento.descricao},
-                            ${lancamento.valor},
-                            ${lancamento.data},
-                          )`)
-    } 
+        let descricaoOriginal = lancamento.descricao;
+        let valor = lancamento.valor;
+        let data = lancamento.data;        
+        lancamentoRepository.save(descricaoOriginal, valor, data);
+    }
        
     // console.log(`\nLançamento:\n`);
     // result = await sql.query(consultarLancamentos);
