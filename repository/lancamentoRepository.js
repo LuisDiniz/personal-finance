97f2454f55c690584b880875f0f522b1c61c6ea3
Luis Diniz <luisdiniz@infosistemas.com.br>
2018-09-18 18:37:19 -0300

    Transforma o objeto "lancamentoRepository" em classe.

--- repository/lancamentoRepository.js
+++ repository/lancamentoRepository.js
@@ -1,33 +1,37 @@
 'use strict';
 
-const sql = require('./util/db');
+const sql = require('../util/db');
 
-async function save (descricao, descricaoOriginal, valor, data) {
-    await sql.query(`INSERT INTO Lancamento (
-                        Descricao,
-                        DescricaoOriginal,
-                        Valor,
-                        Data
-                    )
-                    VALUES (
-                        ${descricao ? descricao : descricaoOrignal},
-                        ${descricaoOrignal},
-                        ${valor},
-                        ${data},
-                    )`);
-}
+module.exports = class LancamentoRepository {
 
-async function findAll () {
-    await sql.query(`SELECT * FROM Lancamento`);
-}
+    async save (descricao, descricaoOriginal, valor, data) {
+        await sql.query(`INSERT INTO Lancamento (
+                            Descricao,
+                            DescricaoOriginal,
+                            Valor,
+                            Data
+                        )
+                        VALUES (
+                            ${descricao ? descricao : descricaoOrignal},
+                            ${descricaoOrignal},
+                            ${valor},
+                            ${data},
+                        )`);
+    }
 
-async function findLancamentoDuplicado (descricaoOrignal, valor, data) {
-    let success = false;
-    let result = await sql.query(`SELECT 1 
-                                FROM Lancamento
-                                WHERE DescricaoOriginial = ${descricaoOrignal}
-                                AND Valor = ${valor}
-                                AND Data = ${data}`);
-    success = result.recordsets.length > 1 ? true : false;
-    return success;                              
+    async findAll () {
+        return await sql.query(`SELECT * FROM Lancamento`);
+    }
+
+    async findLancamentoDuplicado (descricaoOrignal, valor, data) {
+        let success = false;
+        let result = await sql.query(`SELECT 1 
+                                      FROM Lancamento
+                                      WHERE DescricaoOriginial = ${descricaoOrignal}
+                                      AND Valor = ${valor}
+                                      AND Data = ${data}`);
+        success = result.recordsets.length > 1 ? true : false;
+        return success;                              
+    }
+
 }
