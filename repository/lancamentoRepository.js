83d7c70cb3fd8c8e6af0d85283b31221dce737e9
Luis Diniz <luis.diniz.carvalho@gmail.com>
2018-09-20 00:33:05 -0300

    Correções da classe LancamentoRepository.

--- repository/lancamentoRepository.js
+++ repository/lancamentoRepository.js
@@ -4,21 +4,6 @@ const sql = require('../util/db');
 
 module.exports = class LancamentoRepository {
 
-    async save (descricao, descricaoOriginal, valor, data) {
-        await sql.query(`INSERT INTO Lancamento (
-                            Descricao,
-                            DescricaoOriginal,
-                            Valor,
-                            Data
-                        )
-                        VALUES (
-                            ${descricao ? descricao : descricaoOrignal},
-                            ${descricaoOrignal},
-                            ${valor},
-                            ${data},
-                        )`);
-    }
-
     async findAll () {
         return await sql.query(`SELECT * FROM Lancamento`);
     }
@@ -27,11 +12,30 @@ module.exports = class LancamentoRepository {
         let success = false;
         let result = await sql.query(`SELECT 1 
                                       FROM Lancamento
-                                      WHERE DescricaoOriginial = ${descricaoOrignal}
+                                      WHERE DescricaoOriginal = '${descricaoOrignal}'
                                       AND Valor = ${valor}
-                                      AND Data = ${data}`);
-        success = result.recordsets.length > 1 ? true : false;
+                                      AND Data = CONVERT(DATE, '${data}', 103)`);
+        success = result.recordset.length > 0 ? true : false;
         return success;                              
     }
+
+    async save (contaId, descricaoOriginal, valor, data, categoriaId, descricao) {
+        await sql.query(`INSERT INTO Lancamento (
+                            ContaId,
+                            Descricao,
+                            DescricaoOriginal,
+                            Valor,
+                            Data,
+                            CategoriaId
+                        )
+                        VALUES (
+                            ${contaId},
+                            '${descricao ? descricao : descricaoOriginal}',
+                            '${descricaoOriginal}',
+                            ${valor},
+                            CONVERT (DATE, '${data}', 103),
+                            ${categoriaId ? categoriaId : null}
+                        )`);
+    }    
 
 }
