6e8b28fbbc399ca150e2d4da75ba82d194755758
Luis Diniz <luisdiniz@infosistemas.com.br>
2018-09-21 16:09:59 -0300

    Criar classe LancamentoHistoricoCategoria

---
+++ model/lancamentoHistoricoCategoria.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = class LancamentoHistoricoCategoria {
+
+    constructor(){
+        this.lancamentoHistoricoCategoriaId = null;
+        this.descricao = '';
+        this.descricaoParcial = '';
+        this.categoriaId = null;
+    }
+
+}
