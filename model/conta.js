3d298df1035aa6f6d7d92b995a040227599f8544
Luis Diniz <luisdiniz@infosistemas.com.br>
2018-09-21 16:09:46 -0300

    Criar classe Conta

---
+++ model/conta.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = class Conta {
+
+    constructor(){
+        this.contaId = null;
+        this.descricao = '';
+        this.saldo = 0.00;
+        this.manual = true;
+    }
+
+}
