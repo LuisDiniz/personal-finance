96a9432eef63418699d91d5029036e8789ffbc6a
Luis Diniz <luis.diniz.carvalho@gmail.com>
2018-09-21 00:34:13 -0300

    Commit temporario

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 // Imports
 const interImport = require('./import/Inter');
 const formatUtil = require('./util/formatUtil');
-const LancamentoRepository = require('./repository/lancamentoRepository');
+const LancamentoController = require('./controller/lancamentoController');
 const ContaRepository = require('./repository/contaRepository');
-const sql = require('./util/db');
 // Constantes
 const consultarConta = `SELECT * FROM Conta`;
 const filePath = '/Users/Luis/Downloads/inter-extrato.csv'
@@ -14,40 +13,15 @@ async function main() {
 
     let result = {};
 
-    const lancamentoRepository = new LancamentoRepository();
+    const lancamentoController = new LancamentoController();
     const contaRepository = new ContaRepository();
-
-    // Teste de conexão com o banco de dados
-    console.log(`\nContas:\n`);
-    result = await contaRepository.findAll();
-    for (let record of result.recordset) {
-        console.log(record);            
-    }
-
-    // console.log(`\nLançamentos:\n`);
-    // result = await lancamentoRepository.findAll();
-    // for (let record of result.recordset)
-    //     console.log(record);            
+    const CONTA_ID_ITAU = 2;
 
     // Teste importar extrato banco Inter
-    let retorno = await interImport.importFileLancamento(filePath);
-    // Prepara a lista lancamentos que não existem no banco para serem inseridos    
-    let lancamentos = [];
-    for (let lancamento of retorno.lancamentos){
-        let encontrou = await lancamentoRepository.findLancamentoDuplicado(lancamento.descricao.replace(/[']/g, ''), lancamento.valor, lancamento.data);
-        if (!encontrou)
-            lancamentos.push(lancamento);
-    }
+    let retorno = await interImport.importFileLancamento(filePath, CONTA_ID_ITAU);    
+    await lancamentoController.inserirLancamentos(retorno.lancamentos);
     // Persiste as informações
-    let contaId = 2; // Itau
     await contaRepository.atualizarSaldo(contaId, formatUtil.formatReal(retorno.saldo));
-    // Insere os novos lancamentos no banco
-    for (let lancamento of lancamentos){
-        let descricaoOriginal = lancamento.descricao.replace(/[']/g, '');
-        let valor = lancamento.valor;
-        let data = lancamento.data;        
-        await lancamentoRepository.save(contaId, descricaoOriginal, valor, data);
-    }
        
     console.log(`Fim da execução`);
 }
