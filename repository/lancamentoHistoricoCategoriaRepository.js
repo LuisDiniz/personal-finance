8f506f647d633f49b3cbebbe5547b2a270a68ed5
Luis Diniz <luis.diniz.carvalho@gmail.com>
2018-09-26 22:46:36 -0300

    Alterar para utilizar as classes modelos criadas para os objetos.

--- repository/lancamentoHistoricoCategoriaRepository.js
+++ repository/lancamentoHistoricoCategoriaRepository.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const sql = require('../util/db');
+const LancamentoHistoricoCategoria = require('../model/lancamentoHistoricoCategoria');
 
 module.exports = class LancamentoHistoricoCategoriaRepository {
 
     async findAll () {
-        return await sql.query(`SELECT * FROM LancamentoHistoricoCategoria`);
+        let listaLancamentoHistoricoCategoria = [];
+        let result = await sql.query(`SELECT * FROM LancamentoHistoricoCategoria`);
+        for (const record of result.recordset) {
+            let lancamentoHistoricoCategoria = new LancamentoHistoricoCategoria();
+            lancamentoHistoricoCategoria.descricao = record.Descricao;
+            lancamentoHistoricoCategoria.descricaoParcial = record.DescricaoParcial;
+            lancamentoHistoricoCategoria.categoriaId = record.CategoriaId;
+            listaLancamentoHistoricoCategoria.push(lancamentoHistoricoCategoria);
+        }
+        return listaLancamentoHistoricoCategoria;
     }
 
     async save (categoriaId, descricaoParcial, descricao) {
