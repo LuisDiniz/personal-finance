514893fae101abeadb0d5fbc2706659d12cf46c2
Luis Diniz <luis.diniz.carvalho@gmail.com>
2018-09-26 22:47:34 -0300

    Alterar para utilizar as classes modelos criadas para os objetos.

--- controller/lancamentoController.js
+++ controller/lancamentoController.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const LancamentoRepository = require('../repository/lancamentoRepository');
+const LancamentoHistoricoCategoriaRepository = require('../repository/lancamentoHistoricoCategoriaRepository');
 
 module.exports = class LancamentoController {
 
     constructor(){
         this.lancamentoRepository = new LancamentoRepository();
+        this.lancamentoCategoriaRepository = new LancamentoHistoricoCategoriaRepository();
     }
 
     async inserirLancamento(lancamento){
@@ -19,9 +21,21 @@ module.exports = class LancamentoController {
             let encontrou = await this.lancamentoRepository.findLancamentoDuplicado(lancamento);
             if (!encontrou)
                 novosLancamentos.push(lancamento);
-        }    
+        }
+        // Recupera a lista de categoria de lancamentos salvos
+        let listaLancametoCategoria = await this.lancamentoCategoriaRepository.findAll();
         // Insere os novos lancamentos no banco
         for (let lancamento of novosLancamentos){
+            if (lancamento.categoriaId === null){
+                // Atualiza as informações dos lançamentos que casem com alguma descrição parcial salva                               
+                for (let lancamentoCategoria of listaLancametoCategoria){
+                    if (lancamento.descricaoOriginal.match(new RegExp(lancamentoCategoria.descricaoParcial, 'i'))){
+                        lancamento.categoriaId = lancamentoCategoria.categoriaId;
+                        lancamento.descricao = lancamentoCategoria.descricao;
+                        break;
+                    }
+                }
+            }
             await this.inserirLancamento(lancamento);
         }        
     }
